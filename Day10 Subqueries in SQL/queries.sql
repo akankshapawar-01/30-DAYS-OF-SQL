a5a626076871bfd0d74a85c240f709b69fc0bb21
Akanksha Pawar <pawarakanksha1101@gmail.com>
2025-08-12 00:27:47 +0530

    Update queries.sql

--- Day10 Subqueries in SQL/queries.sql
+++ Day10 Subqueries in SQL/queries.sql
@@ -1,1 +1,20 @@
+-- Subquery in SELECT clause
+-- Find each employee’s salary and the company’s average salary in the same row
+SELECT name,salary,
+(SELECT AVG(salary) FROM employees) AS avg_salary
+FROM employees;
 
+-- Subquery in WHERE clause
+-- Find employees who earn more than the average salary
+SELECT name, salary
+FROM employees
+WHERE salary > (SELECT AVG(salary) FROM employees);
+
+-- Subquery in FROM clause
+-- Find the average salary per department using a subquery table
+SELECT dept_id, avg_salary
+FROM (
+    SELECT dept_id, AVG(salary) AS avg_salary
+    FROM employees
+    GROUP BY dept_id
+) AS dept_avg;
